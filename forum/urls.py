b65d60740574a1252b00293f62025cc962c69979
1rahul1 <1rahulprasad1@gmail.com>
2018-11-17 01:15:54 +0530

    add forum templates

--- forum/urls.py
+++ forum/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index)
+    path("",views.index),
+    path("addpost",views.add_post),
+    path("thread",views.thread),
 ]
