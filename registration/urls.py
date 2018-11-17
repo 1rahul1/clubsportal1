a92340d8effe5cbfc1d05d305ef8d31bb8b5b82c
Rohit <rohitsheoran1907@gmail.com>
2018-11-17 14:21:33 +0530

    integration with register login

--- registration/urls.py
+++ registration/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.conf.urls import include
 
 app_name='registration'
 
@@ -8,4 +10,8 @@ urlpatterns = [
     path("register",views.register,name='signup_url'),
     path("otp-verification",views.verify,name='otp_url'),
     path("logout",views.logout,name='logout_url'),
+]
+urlpatterns += [
+    url(r'^propose_join/', include('propose_join.urls', namespace='propose_join')),
+
 ]
