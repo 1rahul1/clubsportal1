7cc16d9e679cbc51a71f77b1e2910325633985f2
Rohit <rohitsheoran1907@gmail.com>
2018-11-18 19:37:46 +0530

    changes applied in models and views of proposejoin app

--- propose_join/urls.py
+++ propose_join/urls.py
@@ -5,5 +5,8 @@ app_name= "propose_join"
 urlpatterns=[
     path('button', views.model_form_upload, name='button'),
     path('list', views.list, name='list'),
+    path('existing',views.existingclub,name='existing'),
+    path('existing/add', views.add_to_join, name='add_to_join'),
+    path('myclub', views.joined, name='myclub')
 
 ]
--- propose_join/views.py
+++ propose_join/views.py
@@ -4,11 +4,11 @@ from .models import ExistingClub,ProposedClub
 from .forms import club_logo
 from django.contrib.auth import get_user_model
 from registration.models import Profile
-
+from django.contrib.auth.models import UserManager
+from django.contrib.auth.decorators import login_required
 User = get_user_model()
+from django.shortcuts import get_object_or_404
 
-
-from .models import CustomUser
 
 def list(request):
     qs = ProposedClub.objects.all()
@@ -24,7 +24,6 @@ def model_form_upload(request):
         if form.is_valid():
             club1 = form.save(commit=False)
             club1.name =Profile.objects.get(user=request.user)
-            #CustomUser.objects.get(Username=request.user)
             club1.save()
             return render(request,'propose_join/next.html')
     else:
@@ -32,3 +31,29 @@ def model_form_upload(request):
     return render(request, 'propose_join/home.html', {
         'form': form
     })
+
+def existingclub(request):
+    mem=request.user
+    qs = ExistingClub.objects.exclude(members=mem)
+    return render(request,"propose_join/existing.html",{"qs":qs})
+
+@login_required
+def add_to_join(request):
+    if request.method == "POST":
+        clubname=request.POST.get('club-joined')
+        print(request.user)
+        cuser = request.user
+        existingclub = ExistingClub.objects.get(club_name=clubname)
+        existingclub.members.add(cuser)
+
+        return redirect("propose_join:existing")
+
+    else:
+
+        return redirect("home:index")
+
+def joined(request):
+    mem=request.user
+    qs=ExistingClub.objects.filter(members=mem)
+    print(qs)
+    return render(request, "propose_join/myclub.html", {"qs": qs})
